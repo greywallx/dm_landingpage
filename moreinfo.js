d9047c27c3c6c493d1a261179ba5b0907c8ad5d8
Sandy Grewal <sandy.grewal17@gmail.com>
2016-07-27 00:26:37 -0400

    moreinfo button working

---
+++ moreinfo.js
@@ -0,0 +1,86 @@
+var checkForm = function(e)
+  {
+    var form = (e.target) ? e.target : e.srcElement;
+    if(form.firstname.value == "") {
+      alert("Please enter your First Name");
+      form.firstname.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+    if(form.lastname.value == "") {
+      alert("Please enter your Last Name");
+      form.lastname.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+    if(form.email.value == "") {
+      alert("Please enter a valid Email address");
+      form.email.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+    if(form.company.value == "") {
+      alert("Please enter Company Name");
+      form.company.focus();
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+      return;
+    }
+  };
+
+
+  var modal_init_info = function() {
+
+    var modalWrapper = document.getElementById("info_wrapper");
+    var modalWindow  = document.getElementById("info_window");
+
+    var openModal = function(e)
+    {
+      modalWrapper.className = "overlay";
+      var overflow = modalWindow.offsetHeight - document.documentElement.clientHeight;
+      if(overflow > 0) {
+        modalWindow.style.maxHeight = (parseInt(window.getComputedStyle(modalWindow).height) - overflow) + "px";
+      }
+      modalWindow.style.marginTop = (-modalWindow.offsetHeight)/2 + "px";
+      modalWindow.style.marginLeft = (-modalWindow.offsetWidth)/2 + "px";
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+
+    var closeModal = function(e)
+    {
+      modalWrapper.className = "";
+      e.preventDefault ? e.preventDefault() : e.returnValue = false;
+    };
+
+    var clickHandler = function(e) {
+      if(!e.target) e.target = e.srcElement;
+      if(e.target.tagName == "DIV") {
+        if(e.target.id != "info_window") closeModal(e);
+      }
+    };
+
+    var keyHandler = function(e) {
+      if(e.keyCode == 27) closeModal(e);
+    };
+
+    if(document.addEventListener) {
+      document.getElementById("info_open").addEventListener("click", openModal, false);
+      document.getElementById("info_close").addEventListener("click", closeModal, false);
+      document.addEventListener("click", clickHandler, false);
+      document.addEventListener("keydown", keyHandler, false);
+    } else {
+      document.getElementById("info_open").attachEvent("onclick", openModal);
+      document.getElementById("info_close").attachEvent("onclick", closeModal);
+      document.attachEvent("onclick", clickHandler);
+      document.attachEvent("onkeydown", keyHandler);
+    }
+
+  };
+
+  if(document.addEventListener) {
+    document.getElementById("modal_feedback").addEventListener("submit", checkForm, false);
+    document.addEventListener("DOMContentLoaded", modal_init_info, false);
+  } else {
+    document.getElementById("modal_feedback").attachEvent("onsubmit", checkForm);
+    window.attachEvent("onload", modal_init_info);
+  }
+
